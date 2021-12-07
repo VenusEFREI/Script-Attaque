788e67d7c25eb9674686abfbe9c97c6e83896c17
BARRY Gael <gael.barry@sfr.com>
2021-12-07 16:45:59 +0100

    Mise en place de la base de données et des payloads

--- models/model_entrées.py
+++ models/model_entrées.py
@@ -1,10 +1,11 @@
-entry =  {
-    "id" : "",
-    "file" : "",
-    "description" : "",
-    "date" : "",
-    "author" : "",
-    "type" : "",
-    "platform" : "",
-    "port" : ""
-}
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.exploit-db.com/exploits/5"
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
+
+content = requests.get(url=url, headers=headers)
+soup = BeautifulSoup(content.text, 'html.parser')
+code = soup.find("pre").find('code').get_text()
+with open("test.c", "w") as f:
+    f.write(code)
